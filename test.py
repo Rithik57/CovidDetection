f6eb801dee4520ceb792cc224cf332888db46942
Rithik Chaudhary <chaudharyrithik57@gmail.com>
2022-08-04 20:36:34 +0530

    save before test spectrogram

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-import os
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
+print("preparing imports ... ")
 import tensorflow as tf
-print(tf.test.gpu_device_name());
-print("CPU count : ",os.cpu_count());
+import os
+import librosa
+print("loading model ... ")
+model = tf.keras.models.load_model('Models/model1.h5')
+print(model.summary())
+print("loading audio file ... ")
+y,sr = librosa.load('test_sound.wav', mono=True, duration=5)
+print("converting to spectrogram ... ")
+import matplotlib.pyplot as plt
+cmap = plt.get_cmap('inferno')
+plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB')
+plt.axis('off')
+plt.savefig(f'test_spectrogram.png')
