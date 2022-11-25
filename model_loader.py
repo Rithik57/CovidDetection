7979918e04e373476395b08a652249706431459a
Rithik Chaudhary <chaudharyrithik57@gmail.com>
2022-11-25 10:07:21 +0530

    jetson script ready in model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,27 +1,29 @@
 # code for the python script in the website is tested here
 print("preparing imports ... ")
-import tensorflow as tf
-import os
+import sklearn
+import pickle
 import librosa
 import numpy as np
+import sounddevice as sd
+from scipy.io.wavfile import write
+import os
+# suppress all warnings
+import warnings
+warnings.filterwarnings("ignore")
+
 print("loading model ... ")
-model = tf.keras.models.load_model('Models/model1.h5')
-print(model.summary())
-print("loading audio file ... ")
-y,sr = librosa.load('test_sound.wav', mono=True, duration=5)
-print("converting to spectrogram ... ")
-import matplotlib.pyplot as plt
-cmap = plt.get_cmap('inferno')
-plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB')
-plt.axis('off')
-plt.savefig(f'test_spectrogram.png')
-header = 'chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
-for i in range(1, 21):
-    header += f' mfcc{i}'
-header += ' label'
-header = header.split()
-print("loading features ... ")
-y,sr = librosa.load('test_sound.wav', mono=True, duration=5)
+clf = pickle.load(open("Models/Model4_GBM.sav",'rb'))
+print("model ready for sampling ...")
+print("listening for cough ...")
+freq=44100 # sampling frequency for the incoming audio
+duration=5 # incoming audio clipped to 5 seconds as per the dataset
+recording = sd.rec(int(duration*freq),samplerate=freq,channels=2) #specify the recording details
+sd.wait() # wait for audio to be recorded
+write("recording0.wav",freq,recording)
+age = int(input("Enter age : "))
+respiratory = int(input("Existing respiratory conditions (1/0) : "))
+path = 'recording0.wav'
+y,sr = librosa.load(path, mono=True, duration=5)
 chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 rmse = librosa.feature.rms(y=y)
 spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
@@ -29,13 +31,19 @@ spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
 rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
 zcr = librosa.feature.zero_crossing_rate(y)
 mfcc = librosa.feature.mfcc(y=y, sr=sr)
-x_test = f'{np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+to_append = f'{np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
 for e in mfcc:
-    x_test += f' {np.mean(e)}'
-x_test = x_test.split()
+    to_append += f' {np.mean(e)}'
+to_append += ' '+str(age)
+to_append += ' '+str(1)
+to_append += ' '+str(respiratory)
+to_append = to_append.split()
+x_test = to_append
 for i in range(len(x_test)) :
     x_test[i] = float(x_test[i])
-print(x_test)
-print("running model ... ")
-preds = model.predict([x_test])
-print(np.argmax(preds))
+preds = clf.predict([x_test])
+if preds[0] == 0:
+    print("Covid negative")
+else :
+    print("Covid Positive")
+os.remove('recording0.wav')
